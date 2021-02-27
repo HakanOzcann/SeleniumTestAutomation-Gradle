3f296714ebcbeca0be305b7c66e744733a283471
HakanOzcann <hakan.ozcan44@hotmail.com>
2021-02-27 23:59:10 +0300

    SeleniumTestAutomation-Gradle

--- src/test/java/Utilities/Driver.java
+++ src/test/java/Utilities/Driver.java
@@ -1,7 +1,11 @@
 package Utilities;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import com.google.common.io.Files;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterSuite;
@@ -17,8 +21,17 @@ public class Driver
         System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/main/resources/geckodriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.spotify.com/tr/");
+
+        File src =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        try {
+            Files.copy(src, new File("./Screenshot/denem2e.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @AfterSuite
@@ -26,5 +39,6 @@ public class Driver
     {
         Driver.driver.quit();
     }
+
 }
 
