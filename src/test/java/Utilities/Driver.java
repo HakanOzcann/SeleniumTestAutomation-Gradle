b65929188b2f75fdfd271c650ba9cf5e8e759ed7
HakanOzcann <hakan.ozcan44@hotmail.com>
2021-02-28 02:02:12 +0300

    SeleniumTestAutomation-Gradle

--- src/test/java/Utilities/Driver.java
+++ src/test/java/Utilities/Driver.java
@@ -3,21 +3,21 @@ package Utilities;
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
-import com.google.common.io.Files;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeSuite;
 
-public class Driver
-{
+public class Driver {
     public static WebDriver driver = null;
 
     @BeforeSuite
-    public void initialize() throws IOException
-    {
+    public void initialize() throws IOException {
         System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/main/resources/geckodriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -25,20 +25,36 @@ public class Driver
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.spotify.com/tr/");
 
-        File src =((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        try {
-            Files.copy(src, new File("./Screenshot/denem2e.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
     }
 
-    @AfterSuite
-    public void TeardownTest()
+    @AfterMethod
+    public void ScreenshotOnFailure(ITestResult result) {
+
+        if (ITestResult.FAILURE == result.getStatus()) {
+
+            try {
+                TakesScreenshot ts = (TakesScreenshot) driver;
+                File source = ts.getScreenshotAs(OutputType.FILE);
+
+                try {
+                    FileHandler.copy(source, new File("/Users/hakanozcan/Desktop/" + result.getName() + ".png"));
+                    System.out.println("Screenshot taken");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } catch (Exception e) {
+                System.out.println("Exception while taking screenshot " + e.getMessage());
+            }
+        }
+    }
+
+    @AfterTest
+    public void CloseDriver()
     {
         Driver.driver.quit();
     }
-
 }
+
+
+
 
